8c078ee5d16518ca4fd456053cc2f1e0f7d489e3
aachal-s <aachal2048@gmail.com>
2024-07-09 10:19:22 +0530

    day 1

---
+++ Day 1 : Arrays/1_SetMatrixZeros.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void setMatrixZero(vector<vector<int>> &arr, int n, int m) {
+    vector<int> row(n, 0), col(m, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] == 1 || col[j] == 1) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    setMatrixZero(arr, n, m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
